e5f3e732196fb8e3c5a33dc7dbcb869dcfa880d2
ulin <outlin2059@outlook.com>
2016-05-11 17:49:22 +0800

    7.3.2 add slug column use my 5 hours to solved this bug

--- rango/admin.py
+++ rango/admin.py
@@ -7,5 +7,8 @@ class PageAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name','views','likes']
 
+# Add in this class to customized the Admin Interface
+    prepopulated_fields = {'slug':('name',)}
+
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Page,PageAdmin)
